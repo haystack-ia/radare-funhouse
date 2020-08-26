4ec89f1988356f089467f39c33a298b0683b1f32
haystack <haystack@lavabit.com>
2020-08-26 16:55:07 -0400

    Add fixup for PLT import errors
    Added plt_fixup.py to bandaid the issue reported @
    https://github.com/radareorg/radare2/issues/17523
    
    Will only work on ARM. Tried to make it generic but aav strikes again.
    
    Also added r2p_helpers.py, which provides some function decorators
    for r2pipe scripts. They will wrap a function and automatically
    save & restore certain parts of the r2 state. For example, using
    the @preserve_position decorator will return you to the original
    file position after the function runs.

---
+++ plt_fixup.py
@@ -0,0 +1,86 @@
+import r2pipe
+from r2p_helpers import preserve_esil_registers,\
+                        preserve_flagspace,\
+                        preserve_position
+
+@preserve_esil_registers
+@preserve_flagspace
+@preserve_position
+def rename_stubs(r, f):
+    '''
+    calculate proper function name for reloc stub.
+
+    Arguments:
+      r: r2pipe() object
+      f: json representation of function (from aflj)
+    '''
+
+    #if name starts with 'sym.imp' it's already been named
+    if f['name'].startswith('sym.imp'):
+        r.cmd('?e Don\'t need to rename {}'.format(f['name']))
+        return
+
+    #setup analysis
+    r.cmd('s {}'.format(f['offset']))
+    #setup ESIL
+    r.cmd('aeip; aeim')
+
+    #execute until final instruction
+    while True:#lol
+        current_instr = r.cmdj('pdj1 @ pc')[0]
+        #I used to check if offset = fcn_last but aav flags fuck that up
+        if current_instr["opcode"].startswith('ldr pc'):
+            break
+        else:
+            r.cmd('aes')
+
+    #exploit the format of the ESIL. Probably only works for ARM
+    got_offset = int(r.cmdj('pdj1 @ pc')[0]['esil'].split(",")[0])
+    ip = r.cmdj('aerj')['ip']
+    reloc_addr = got_offset + ip
+    r.cmd('fs relocs')
+
+    flag = [f for f in r.cmdj('fj') if f['offset'] == reloc_addr]
+    if len(flag) < 1:
+        r.cmd("?e no reloc found @ {}".format(hex(reloc_addr)))
+    else:
+        fname = flag[0]['name'][6:]
+        r.cmd('afn sym.imp.{}'.format(fname))
+
+
+
+
+
+
+
+
+r = r2pipe.open()
+
+############################
+#get bounds of .plt section#
+############################
+
+sections = r.cmdj('iSj')
+
+#find section named '.plt'.
+#assume there's only one
+plts = [s for s in sections if s['name']=='.plt']
+if len(plts) < 1:
+    r.cmd('?e no .plt section found.')
+    exit()
+else:
+    plt = plts[0]
+    plt_start = plt['vaddr']
+    plt_size = plt['vsize']
+    plt_end = plt_start + plt_size
+
+#get list of functions
+all_functions = r.cmdj('aflj')
+
+#use f['offset'] > plt_start instead of f['offset'] >=plt_start
+#to exclude the resolver function at the top of .plt
+plt_stubs = [f for f in all_functions if f['offset'] > plt_start\
+             and f['offset'] < plt_end]
+
+for stub in plt_stubs:
+    rename_stubs(r, stub)
---
+++ r2p_helpers.py
@@ -0,0 +1,83 @@
+import r2pipe
+
+#I copy-pasted this from stack overflow
+#thanks Boaz Yaniv!
+def decdec(inner_dec):
+    def ddmain(outer_dec):
+        def decwrapper(f):
+            wrapped = inner_dec(outer_dec(f))
+            def fwrapper(*args, **kwargs):
+               return wrapped(*args, **kwargs)
+            return fwrapper
+        return decwrapper
+    return ddmain
+
+
+
+def must_have_r2pipe_arg(func):
+    '''
+    This decorator is ONLY to be used to wrap other r2pipe helper decorators.
+
+    Check positional args to make sure that one of them is an r2pipe instance,
+    then add the r2pipe instance to the front of the child function's argument
+    list. Need this for the other wrappers since they use an r2pipe instance passed
+    to the wrapped function.
+
+    It was at this point that I realized that writing these little helper
+    functions was probably more trouble than it was worth.
+    '''
+    def must_have_r2pipe_arg_wrapper(*args, **kwargs):
+
+        r2pipe_types = [r2pipe.open_sync.open,
+                        r2pipe.open_sync.open]
+
+        r2pipe_arg = None
+        for a in args:
+            if type(a) in r2pipe_types:
+                r2pipe_arg = a
+                break
+
+        if r2pipe_arg is None:
+            print(args)
+            raise Exception("A positional argument needs to be a\
+            r2pipe instance to use this wrapper")
+
+        else:
+            func(r2pipe_arg, *args, **kwargs)
+
+    return must_have_r2pipe_arg_wrapper
+
+@decdec(must_have_r2pipe_arg)
+def preserve_position(func):
+    '''
+    decorator that preserves the seek position before function calls.
+    '''
+    def preserve_pos_wrapper(r, *args, **kwargs):
+        old_pos = int(r.cmd('s'), 16)
+        func(*args, **kwargs)
+        r.cmd('s {}'.format(old_pos))
+
+    return preserve_pos_wrapper
+
+@decdec(must_have_r2pipe_arg)
+def preserve_esil_registers(func):
+    '''
+    decorator that preserves ESIL registers
+    '''
+    def preserve_er_wrapper(r, *args, **kwargs):
+        old_aers = r.cmdj('aerj')
+        #clear registers for enclosed function
+        r.cmd('aer0')
+        func(*args, **kwargs)
+        for reg,val in old_aers.items():
+            r.cmd('aer {}={}'.format(reg, val))
+    return preserve_er_wrapper
+
+@decdec(must_have_r2pipe_arg)
+def preserve_flagspace(func):
+    def preserve_fs_wrapper(r, *args, **kwargs):
+        old_fs = r.cmdj('fssj')[-1]['name']
+        func(*args, **kwargs)
+        r.cmd('fs {}'.format(old_fs))
+    return preserve_fs_wrapper
+
